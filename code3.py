8b2239eb73c76bbd91b4a1fe7f9da65d6567898d
abhash <abhashkumar199384@gmail.com>
2025-04-26 12:14:59 +0530

    c4

--- code3.py
+++ code3.py
@@ -1,2 +1,27 @@
+import boto3
+
 hdhcdh
 hdhhdhhd
+def launch_ec2_instance():
+    client = boto3.client('cloudformation')
+
+    template_body = """
+    AWSTemplateFormatVersion: '2010-09-09'
+    Resources:
+      MyEC2Instance:
+        Type: 'AWS::EC2::Instance'
+        Properties:
+          InstanceType: t2.micro
+          ImageId: ami-0abcdef1234567890  # Replace with a valid AMI ID
+          KeyName: my-key-pair          # Replace with your key pair name
+    """
+
+    response = client.create_stack(
+        StackName='MyEC2Stack',
+        TemplateBody=template_body
+    )
+
+    return response
+
+# Call the function to launch the EC2 instance
+launch_ec2_instance()
